a7d547193a944846bf0a08d6582dd95a89af8e38
Aaryaman Maheshwari <67153585+fullstackslayer@users.noreply.github.com>
2021-10-10 03:45:13 +0530

    Add create post button

---
+++ components/university/CreatePostButton.tsx
@@ -0,0 +1,17 @@
+import { Button } from "../Button";
+
+export const CreatePostButton = () => {
+  return (
+    <Button
+      bg="bg-[#fff]"
+      height="h-[2.5rem]"
+      width="w-full"
+      className="!rounded-full text-bg"
+    >
+      <div className="mx-auto flex items-center gap-2">
+        <img src="/icons/plus.svg" alt="" className="h-[1.3rem] w-[1.3rem]" />
+        Create Post
+      </div>
+    </Button>
+  );
+};
--- components/university/UniDetailsSection.tsx
+++ components/university/UniDetailsSection.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { UniversityEntity } from "../../entities/UniversityEntity";
+import { CreatePostButton } from "./CreatePostButton";
 
 export const UniDetailsSection = ({
   university,
@@ -37,10 +38,10 @@ export const UniDetailsSection = ({
         </span>
       </div>
 
-      {/* CREATE POST BUTTON
-      <div className="flex items-center justify-between pb-4 border-b border-bgVariant2">
-
-      </div> */}
+      {/* CREATE POST BUTTON */}
+      <div className="flex items-center justify-between">
+        <CreatePostButton />
+      </div>
     </div>
   );
 };
