81ac0439ebce474d0db41ad7caa68bf6e7f0d10e
Aaryaman Maheshwari <67153585+fullstackslayer@users.noreply.github.com>
2021-10-09 23:17:05 +0530

    Finish Banner for Uni Page

---
+++ components/Loader.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Loader = () => {
+  return (
+    <h1
+      className="md:text-[2rem] xl:text-[2.5rem] font-extrabold"
+      style={{ fontFamily: "'Poppins', sans-serif" }}
+    >
+      Loading...
+    </h1>
+  );
+};
