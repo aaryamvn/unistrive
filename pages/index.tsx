9bcf6294a37fee59cb241546b1091cbe060c9be4
samrath2007 <samrath1324@gmail.com>
2021-10-09 15:11:22 +0530

    abno is dum, i am smart

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,22 +6,26 @@ const Index = () => {
   const { user, logout } = useAuthContext();
   return (
     <Layout containerClassName="relative container mx-auto flex items-center justify-center">
-      <div className="md:w-[30rem] xl:w-[40rem] flex flex-col gap-[1rem]">
-        <h1
-          className="md:text-[2rem] xl:text-[2.5rem] font-extrabold"
-          style={{ fontFamily: "'Poppins', sans-serif" }}
-        >
-          Welcome {user?.displayName}
-        </h1>
-        <Button bg="bg-accent1" width="w-[13rem]" onClick={logout}>
-          Logout
-        </Button>
+      <div className="relative h-[calc(100vh-5.4rem)] w-screen container mx-auto flex items-center justify-center">
+        <div className="md:w-[30rem] xl:w-[40rem] flex flex-col gap-[1rem]">
+          <h1
+            className="md:text-[2rem] xl:text-[2.5rem] font-extrabold"
+            style={{ fontFamily: "'Poppins', sans-serif" }}
+          >
+            Welcome {user.displayName}
+          </h1>
+
+          <Button bg="bg-accent1" width="w-[8rem]" onClick={logout}>
+            <h3>Logout</h3>
+          </Button>
+        </div>
+
+        <img
+          src="/call_hand_vector.png"
+          className="absolute bottom-0 right-5"
+          alt=""
+        />
       </div>
-      <img
-        src="/call_hand_vector.png"
-        className="absolute bottom-0 right-5"
-        alt=""
-      />
     </Layout>
   );
 };
