93c2309bec78d0dfa2e5f276e184b8b98950a407
Aaryaman Maheshwari <67153585+fullstackslayer@users.noreply.github.com>
2021-10-10 03:19:16 +0530

    add about to uni details card

--- components/university/UniDetailsSection.tsx
+++ components/university/UniDetailsSection.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { UniversityEntity } from "../../entities/UniversityEntity";
 
-export const UniDetailsSection = () => {
+export const UniDetailsSection = ({
+  university,
+}: {
+  university: UniversityEntity;
+}) => {
   return (
-    <div className="w-[19.5rem] rounded-md bg-bgVariant1 p-5 flex flex-col items-center gap-4 text-center"></div>
+    <div className="w-[19.5rem] rounded-md bg-bgVariant1 p-5 flex flex-col gap-4">
+      {/* ABOUT */}
+      <div className="flex flex-col gap-1">
+        <h3 className="capitalize font-bold text-muted1 text-md">
+          About The University
+        </h3>
+        <p className="font-regular text-muted1 text-sm">
+          {university?.bio ||
+            `Looks like this university hasn't put up a bio yet!`}
+        </p>
+      </div>
+
+      {/* MEMBERS */}
+    </div>
   );
 };
