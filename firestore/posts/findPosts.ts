9391d4184e2ffa93b7acaf2279c50124df04305a
Aaryaman Maheshwari <67153585+fullstackslayer@users.noreply.github.com>
2021-10-10 11:59:31 +0530

    fix promise stuff for findPosts

--- firestore/posts/findPosts.ts
+++ firestore/posts/findPosts.ts
@@ -1,4 +1,3 @@
-// This finds all posts for the user
 import { findPostsByUniversity } from "./findPostsByUniversity";
 import { findUserById } from "../users/findUserById";
 import { PostEntity } from "../../entities/PostEntity";
@@ -8,12 +7,18 @@ export const findPosts = async (userId: string): Promise<PostEntity[]> => {
   const posts: PostEntity[] = [];
 
   // This retrieves all posts from all universities the user is following
-  user?.followingUniNames?.map(async (uniName: string) => {
+  let unresolved = user?.followingUniNames?.map(async (uniName: string) => {
     const uniPosts = await findPostsByUniversity(uniName);
-    uniPosts.map((post: PostEntity) => {
+
+    uniPosts.forEach((post: PostEntity) => {
       posts.push(post);
+      console.log(posts);
     });
   });
 
+  // wait for all the posts to get pushed
+  await Promise.all(unresolved);
+
+  console.log(posts);
   return posts as any;
 };
