9c528501fdab6de88f77381c072906d50d1f644b
samrath2007 <samrath1324@gmail.com>
2021-10-10 13:20:11 +0530

    fix unicoin[wip]

--- entities/ConsultantProfileEntity.ts
+++ entities/ConsultantProfileEntity.ts
@@ -1,7 +1,7 @@
 export interface ConsultantProfileEntity {
   id?: string;
   userId: string; // id of the user this profile belongs too
-
+  documentId: string;
   universityName: string; // the name of the university they are a student of (actually matched in db)
   courseName: string; // the name of the course they are majoring in at their university (hardcoded string)
   createdCommentIds?: string[]; // the comments they have written on the postings of highschoolers
