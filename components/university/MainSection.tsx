3d2bf9d3b80e43a5b688a8d0a91d45e2bbb5468b
Aaryaman Maheshwari <67153585+fullstackslayer@users.noreply.github.com>
2021-10-10 07:29:21 +0530

    hello

--- components/university/MainSection.tsx
+++ components/university/MainSection.tsx
@@ -1,24 +1,18 @@
 import React from "react";
 import { PostEntity } from "../../entities/PostEntity";
-import { findUniversityByName } from "../../firestore/universities/findUniversityByName";
-import { findUserById } from "../../firestore/users/findUserById";
 import { Post } from "../Post";
 
 export const MainSection = ({ posts }: { posts: PostEntity[] }) => {
   return (
     <div className="w-[40rem]">
-      {posts.map(async (post) => {
-        const postCreator = await findUserById(post?.creatorId);
-        const postUniversity = await findUniversityByName(post?.universityName);
-
+      {posts.map((post) => {
         return (
           <Post
             id={post?.id}
             title={post?.title}
             content={post?.content}
-            creator={postCreator}
+            creatorId={post.creatorId}
             universityName={post?.universityName}
-            universityLogoUrl={postUniversity?.logoUrl}
             upvotesAmt={post?.upvoterIds?.length}
           />
         );
