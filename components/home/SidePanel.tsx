5a0c6e01f6c7a94e418e795fdbc737c0459eb3d3
Aaryaman Maheshwari <67153585+fullstackslayer@users.noreply.github.com>
2021-10-10 14:09:15 +0530

    Update SidePanel.tsx

--- components/home/SidePanel.tsx
+++ components/home/SidePanel.tsx
@@ -3,7 +3,52 @@ import React from "react";
 export const SidePanel = () => {
   return (
     <div className="w-[19.5rem]">
-      <div className="rounded-md bg-bgVariant1 p-5 flex flex-col gap-4"></div>
+      <div className="rounded-md bg-bgVariant1 p-5 flex flex-col gap-4">
+        <h3 className="capitalize font-bold text-muted1 text-md truncate">
+          Relevant Universities
+        </h3>
+      </div>
+    </div>
+  );
+};
+
+const University = ({
+  id,
+  name,
+  logoUrl,
+  followerCount,
+  studentCount,
+  isFinalUniOfList,
+}) => {
+  return (
+    <div
+      className={`flex items-center justify-between w-full ${
+        !isFinalUniOfList && "pb-2 border-b border-bgVariant2"
+      }`}
+    >
+      <div className="flex items-center gap-2">
+        <img
+          src={logoUrl}
+          alt=""
+          className="h-10 w-10 object-cover rounded-md"
+        />
+
+        <div className="flex flex-col gap-1">
+          <h3 className="font-bold text-lg truncate">{name}</h3>
+          <div className="flex items-center gap-3">
+            <span>
+              {followerCount} <h5 className="font-semibold">Followers</h5>
+            </span>
+
+            <span>
+              {studentCount} <h5 className="font-semibold">Students</h5>
+            </span>
+          </div>
+        </div>
+      </div>
+
+      {/* Follow Button */}
+      <div>follohw</div>
     </div>
   );
 };
