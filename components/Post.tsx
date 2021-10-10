463077dd4da397cc86c315cafcba52f28ff60492
Aaryaman Maheshwari <67153585+fullstackslayer@users.noreply.github.com>
2021-10-10 06:41:05 +0530

    start post

--- components/Post.tsx
+++ components/Post.tsx
@@ -6,10 +6,10 @@ interface PostProps {
   content: string;
   creator: UserEntity;
   universityName: string;
+  universityLogoUrl: string;
   upvotesAmt?: number;
   commentsAmt?: number;
   showCommentsButton?: boolean;
-  createdAt: Date;
 }
 
 export const Post: React.FC<PostProps> = ({
@@ -17,20 +17,48 @@ export const Post: React.FC<PostProps> = ({
   content,
   creator,
   universityName,
-  createdAt,
+  universityLogoUrl,
   title,
   showCommentsButton = true,
   upvotesAmt,
 }) => {
   return (
-    <div className="w-full bg-bgVariant1 flex gap-2">
+    <div className="w-full rounded-md p-4 bg-bgVariant1 flex items-center gap-4">
       {/* Upvotes */}
-      <div></div>
+      <div className="flex flex-col items-center gap-2">
+        <img
+          src="/icons/upvote.svg"
+          alt=""
+          className="h-2 w-2 text-white fill-current"
+        />
+        <h3 className="text-sm font-semibold">{upvotesAmt}</h3>
+      </div>
 
-      <div>
+      <div className="flex flex-col gap-1">
         {/* Header */}
+        <div className="flex items-center gap-3">
+          {/* university */}
+          <div className="flex items-center">
+            <img
+              src={universityLogoUrl}
+              alt=""
+              className="h-5 w-5 object-cover rounded-sm mr-1"
+            />
+            <span className="font-semibold">{universityName}</span>
+          </div>
+
+          {/* user */}
+          <span>
+            Posted by <span className="font-semibold">{creator?.username}</span>
+          </span>
+        </div>
+
         {/* Title */}
+        <h1 className="text-lg font-bold trunctate">{title}</h1>
+
         {/* Content */}
+        <p className="text-muted1 text-md font-regular">{content}</p>
+
         {/* Comments Button */}
       </div>
     </div>
