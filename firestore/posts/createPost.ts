220767015679da84d71a263fcc5e18d4f0e68904
Aaryaman Maheshwari <67153585+fullstackslayer@users.noreply.github.com>
2021-10-10 10:51:23 +0530

    fix yet another samsam classic

--- firestore/posts/createPost.ts
+++ firestore/posts/createPost.ts
@@ -3,15 +3,27 @@ import { postsCollection } from "../collections";
 import { editHighschoolerProfile } from "../highschoolerProfiles/editHighschoolerProfile";
 import { findHighschoolerProfileById } from "../highschoolerProfiles/findHighschoolerProfileById";
 
+import { findUserById } from "../users/findUserById";
+
 export const createPost = async (data: PostEntity): Promise<string> => {
-  const doc = await postsCollection.add(data);
-  postsCollection.doc(doc.id).update({ id: doc.id });
+  const creator = await findUserById(data.creatorId);
 
-  const highSchoolStudent = await findHighschoolerProfileById(data.creatorId);
-    let postsByStudent = highSchoolStudent.createdPostIds;
-    postsByStudent.push(doc.id);
+  if (creator.accountType === "highschooler") {
+    // create the post
+    const doc = await postsCollection.add(data);
+    postsCollection.doc(doc.id).update({ id: doc.id });
 
-    editHighschoolerProfile({ createdPostIds: postsByStudent }, data.creatorId);
+    // push it to the users post array
+    const profileId = creator.highschoolerProfileId;
+    const highSchoolerProfile = await findHighschoolerProfileById(profileId);
+
+    const postsByHighschooler = highSchoolerProfile?.createdPostIds || [];
+    postsByHighschooler.push(doc.id);
+
+    editHighschoolerProfile({ createdPostIds: postsByHighschooler }, profileId);
+
+    // return the new post
     console.log(doc);
-  return doc.id;
+    return doc.id;
+  }
 };
