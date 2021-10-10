30cd4a05a846c2a961dce5ce9964d2f5cd44f26f
samrath2007 <samrath1324@gmail.com>
2021-10-10 12:46:20 +0530

    remove docId

--- firestore/consultantProfiles/createConsultantProfile.ts
+++ firestore/consultantProfiles/createConsultantProfile.ts
@@ -5,7 +5,6 @@ export const createConsultantProfile = async (
   data: ConsultantProfileEntity,
 ) => {
   const doc = await (await consultantProfilesCollection.add(data)).get();
-  consultantProfilesCollection.doc(doc.id).update({ docId: doc.id });
   console.log(doc);
 
   return {
