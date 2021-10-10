45c8964ed1f5cc93d0c4ca1bf03fd4688887ecab
Aaryaman Maheshwari <67153585+fullstackslayer@users.noreply.github.com>
2021-10-10 06:49:08 +0530

    fix sammys messup with overwriting docs pt 2

--- firestore/universities/createUniversity.ts
+++ firestore/universities/createUniversity.ts
@@ -3,8 +3,8 @@ import { universitiesCollection } from "../collections";
 
 export const createUniversity = async (data: UniversityEntity) => {
   const doc = await universitiesCollection.add(data);
-  universitiesCollection.doc(doc.id).set({ id: doc.id });
-  console.log(doc);
+  universitiesCollection.doc(doc.id).update({ id: doc.id });
 
+  console.log(doc);
   return doc;
 };
