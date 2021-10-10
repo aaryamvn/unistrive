e633077f1ea335526f914688dd90f9b7e900a790
Divy Srivastava <dj.srivastava23@gmail.com>
2021-10-10 12:17:25 +0530

    fix findPosts

--- firestore/posts/findPosts.ts
+++ firestore/posts/findPosts.ts
@@ -4,21 +4,19 @@ import { PostEntity } from "../../entities/PostEntity";
 
 export const findPosts = async (userId: string): Promise<PostEntity[]> => {
   const user = await findUserById(userId);
-  const posts: PostEntity[] = [];
 
   // This retrieves all posts from all universities the user is following
-  let unresolved = user?.followingUniNames?.map(async (uniName: string) => {
-    const uniPosts = await findPostsByUniversity(uniName);
+  let unresolvedPosts: Promise<PostEntity[]>[] = user?.followingUniNames?.map(
+    async (uniName: string) => {
+      const uniPosts = await findPostsByUniversity(uniName);
+      return uniPosts;
+    },
+  );
 
-    uniPosts.forEach((post: PostEntity) => {
-      posts.push(post);
-      console.log(posts);
-    });
-  });
+  if (!unresolvedPosts) return [];
 
   // wait for all the posts to get pushed
-  await Promise.all(unresolved);
+  let posts = (await Promise.all(unresolvedPosts)).flat();
 
-  console.log(posts);
   return posts as any;
 };
