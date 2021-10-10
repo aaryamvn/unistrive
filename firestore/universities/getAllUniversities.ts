33ca1a729348e04043e4fc3603ec81f2c05df9ee
samrath2007 <samrath1324@gmail.com>
2021-10-10 14:26:08 +0530

    init basic college fetching

---
+++ firestore/universities/getAllUniversities.ts
@@ -0,0 +1,13 @@
+import { UniversityEntity } from "../../entities/UniversityEntity";
+import { universitiesCollection } from "../collections";
+
+export const getAllUniversities = async (): Promise<UniversityEntity[]> => {
+  const unis = (await universitiesCollection.get()).docs;
+
+  let filteredUnis: UniversityEntity[]
+  unis.map((uni) => {
+    filteredUnis.push(uni.data() as UniversityEntity);
+  });
+
+  return filteredUnis as any;
+};
---
+++ pages/colleges.tsx
@@ -0,0 +1,37 @@
+import {  useState } from "react";
+import { Navbar } from "../components/Navbar";
+import { UniversityEntity } from "../entities/UniversityEntity";
+import { getAllUniversities } from "../firestore/universities/getAllUniversities";
+
+export const Login = () => {
+  const [universities, setUniversities] = useState<UniversityEntity[]>();
+
+  const getUniversities = async () => {
+    const universities = await getAllUniversities();
+    setUniversities(universities);
+  };
+
+  getUniversities()
+
+  return (
+    <>
+      <Navbar />
+      <div className="relative h-screen mx-auto flex items-center justify-center">
+        <div className="md:w-[30rem] xl:w-[40rem] flex flex-col gap-[1rem]">
+          <h1
+            className="md:text-[2rem] xl:text-[2.5rem] font-extrabold"
+            style={{ fontFamily: "'Poppins', sans-serif" }}
+          >
+            Colleges
+          </h1>
+
+          <p className="text-muted1 md:text-[1.2rem] xl:text-[1.4rem]">
+            Here is a list of colleges who have registered with Unistrive
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
