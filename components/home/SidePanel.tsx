61f961f56a9973a7606eede9e288dc203e793ec7
Aaryaman Maheshwari <67153585+fullstackslayer@users.noreply.github.com>
2021-10-10 13:48:44 +0530

    start sidePanel

---
+++ components/home/SidePanel.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const SidePanel = () => {
+  return (
+    <div className="w-[19.5rem]">
+      <div className="rounded-md bg-bgVariant1 p-5 flex flex-col gap-4"></div>
+    </div>
+  );
+};
