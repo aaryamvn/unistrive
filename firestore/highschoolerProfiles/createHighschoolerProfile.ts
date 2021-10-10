e6fd39c40578af865428ef2d1ac96821a99662e8
samrath2007 <samrath1324@gmail.com>
2021-10-10 12:43:58 +0530

    add docIds

--- firestore/highschoolerProfiles/createHighschoolerProfile.ts
+++ firestore/highschoolerProfiles/createHighschoolerProfile.ts
@@ -5,6 +5,7 @@ export const createHighschoolerProfile = async (
   data: HighschoolerProfileEntity,
 ) => {
   const doc = await (await highschoolerProfilesCollection.add(data)).get();
+  highschoolerProfilesCollection.doc(doc.id).update({ docId: doc.id });
   console.log(doc);
 
   return {
